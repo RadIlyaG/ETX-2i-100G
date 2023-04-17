733f988bdb9d83a7fe28464cdfa70735f3310732
Ilya Ginzburg <ilya_g@rad.com>
2023-04-17 11:41:22 +0300

    Update HWinit of ATE-1

--- AT-ETX-2-100G-4Q/software/AT-2-100G-1-W10/HWinit.tcl
+++ AT-ETX-2-100G-4Q/software/AT-2-100G-1-W10/HWinit.tcl
@@ -1,8 +1,8 @@
 set gaSet(javaLocation) C:\\Program\ Files\\Java\\jre1.8.0_181\\bin
 switch -exact -- $gaSet(pair) {
   1 - 5 - SE {
-      set gaSet(comDut)     4
-      set gaSet(comAux)   2
+      set gaSet(comDut)  2
+      set gaSet(comAux)  6
       console eval {wm geometry . +150+1}
       console eval {wm title . "Con 1"} 
       set gaSet(pioBoxSerNum) FT31CP7A  
